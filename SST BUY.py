fb94956053d6a4dee84fc21b0f2b2a569e589945
Adiversion <99112612+Adiversion@users.noreply.github.com>
2022-07-21 11:08:31 +0530

    Update SST BUY.py

--- SST BUY.py
+++ SST BUY.py
@@ -39,7 +39,7 @@ df = pd.DataFrame(positions['data'])
 df = df[df['symbol'].isin(portfolio.keys())][['symbol', 'lastPrice']]
 df['bought_price'] = df['symbol'].map(portfolio)
 df['diff_price'] = (df['lastPrice'].astype(float) - df['bought_price'].astype(float))
-df['percentage_changes'] = (df['bought_price'].astype(float) - df['diff_price'].astype(float))/10
+df['percentage_changes'] = (df['bought_price'].astype(float) / df['diff_price'].astype(float))/10
 # Format the percentages changes to display 2 decimal places
 df['percentage_changes'] = df['percentage_changes'].apply(
                                 lambda x: '{0:.2f}%'.format(x))
